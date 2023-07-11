7d7b76c2c9cbf14dbe0081c05cf707ddf26b394a
Alex <68423461+ReikTorrek@users.noreply.github.com>
2023-07-11 21:17:13 +0300

    add small pop-up with dice assets on character page

--- html/js/characterMain.js
+++ html/js/characterMain.js
@@ -7,4 +7,24 @@ $(document).ready(function () {
             $("#" + blockId).show();
         }
     })
+
+    let panelVisible = false;
+
+    $("#togglePanelBtn").click(function() {
+        let panelWidth;
+        if (panelVisible) {
+            panelWidth = $("#myPanel").outerWidth();
+            $("#togglePanelBtn").animate({ right: "-=" + panelWidth }, function() {
+                $("#myPanel").removeClass("show");
+            });
+            $("#togglePanelBtn").text('<')
+        } else {
+            $("#myPanel").addClass("show");
+            panelWidth = $("#myPanel").outerWidth();
+            $("#togglePanelBtn").animate({ right: "+=" + panelWidth });
+            $("#togglePanelBtn").text('>')
+        }
+
+        panelVisible = !panelVisible;
+    });
 })
--- html/modules/character/view/characterView.php
+++ html/modules/character/view/characterView.php
@@ -1,15 +1,28 @@
 <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <button class="btn btn-primary fixed-button" id="togglePanelBtn"><</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel" id="myPanel">
+                <div class="dice-icons">
+                    <span>4</span>
+                    <span>6</span>
+                    <span>8</span>
+                    <span>10</span>
+                    <span>12</span>
+                    <span>20</span>
+                </div>
+            </div>
+        </div>
+    </div>
     <h1><?=$data->name?></h1>
     <?php if (is_array($data->custom_fields)): foreach ($data->custom_fields as $key => $custom_field):?>
         <h2 class="block_name" style="color: <?= $custom_field['color'] ?>" data-id="<?= $custom_field['header_id'] ?>"><?=$key?></h2>
         <div id="<?=$custom_field['header_id']?>">
             <?php foreach ($custom_field as $value): if (is_array($value)): ?>
-<!--                    <dl>
-                        <dt style="color: <?php /*=$value['color']*/?>">
-                            <?php /*=$value['name']*/?>
-                        </dt>
-                        <dd style="color: <?php /*=$value['data_color']*/?>"><?php /*=$value['data']*/?></dd>
-                    </dl>-->
             <p>
                 <span style="color: <?=$value['color']?>"><?=$value['name']?>: </span>
                 <span style="color: <?=$value['data_color']?>"><?=$value['data']?></span>
